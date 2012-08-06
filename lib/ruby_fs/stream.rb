7915372a80eb2910c2266fa96d0fa0fad750374c
Ben Langfeld <ben@langfeld.me>
2012-08-06 14:31:14 +0100

    [BUGFIX] Treat connection refused errors as a disconnection

--- lib/ruby_fs/stream.rb
+++ lib/ruby_fs/stream.rb
@@ -35,7 +35,7 @@ module RubyFS
       @socket = TCPSocket.from_ruby_socket ::TCPSocket.new(@host, @port)
       post_init
       loop { receive_data @socket.readpartial(4096) }
-    rescue EOFError, IOError
+    rescue EOFError, IOError, Errno::ECONNREFUSED
       logger.info "Client socket closed!"
       terminate
     end
