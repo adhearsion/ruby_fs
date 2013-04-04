e416b744fa4061214e25711c74c7c4a457c95d85
Ben Langfeld <ben@langfeld.me>
2013-04-03 21:54:13 -0300

    [DOC] Doc format typo

--- lib/ruby_fs/event.rb
+++ lib/ruby_fs/event.rb
@@ -3,7 +3,7 @@ require 'ruby_fs/response'
 module RubyFS
   class Event < Response
     #
-    # @return [String[ The name of the event
+    # @return [String] The name of the event
     def event_name
       content[:event_name]
     end
